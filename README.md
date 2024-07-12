c65e8da579d4f973abbf67fe7bfaf192563ff549
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2024-07-12 11:01:41 -0300

    Adicionando o README.md

---
+++ README.md
@@ -0,0 +1,29 @@
+# plann.er
+
+Consiste em um site para a organização de viagens. Feito seguindo o evento da **NLW Journey** da **Rocketseat**.
+
+![Home](./images/home.png)
+
+![Trip details](./images/trip-details.png)
+
+## Tecnologias
+
+![Typescript](https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white)
+
+![React.js](https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB)![TailwindCSS](https://img.shields.io/badge/Tailwind_CSS-38B2AC?style=for-the-badge&logo=tailwind-css&logoColor=white)![Vite](https://img.shields.io/badge/vite-%23646CFF.svg?style=for-the-badge&logo=vite&logoColor=white)
+
+![Node.js](https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white)![Zod](https://img.shields.io/badge/zod-%233068b7.svg?style=for-the-badge&logo=zod&logoColor=white)![SQLite](https://img.shields.io/badge/SQLite-07405E?style=for-the-badge&logo=sqlite&logoColor=whit)![Prisma](https://img.shields.io/badge/Prisma-3982CE?style=for-the-badge&logo=Prisma&logoColor=white)
+
+## Funcionalidades
+
+- Criação de uma viagem com destino, data de início e término e convidados
+- Adicionar atividades para essa viagem, com data e horário
+- Confirmação de convidados via email
+
+## Como executar
+
+- Baixe o projeto em .zip e extraia seus arquivos ou clone ele: `git clone `
+- Em cada pasta (_client_ e _server_) rode o comando `npm install` em seu terminal para instalar as dependências do projeto
+- Crie um arquivo .env e siga o exemplo (.env.example) para colocar as informações corretamente
+- Inicie o servidor backend, a API, através do comando `npm run dev` na pasta _server_
+- Inicie o _vite_ com o comando `npm run dev` na pasta _client_
--- client/src/pages/trip-details/index.tsx
+++ client/src/pages/trip-details/index.tsx
@@ -5,15 +5,10 @@ import { ImportantLinks } from "./components/layout/menu/important-links";
 import { Guests } from "./components/layout/menu/guests";
 import { DestinationAndDateHeader } from "./components/layout/destination-and-date-header";
 import { Activities } from "./components/activities";
-import { UpdateTripDestinationAndDateModal } from "./components/modals/update-trip-destination-and-date-modal";
 
 export function TripDetailsPage() {
 	const [isCreateActivityModalOpen, setIsCreateActivityModalOpen] =
 		useState(false);
-	const [
-		isUpdateTripDestinationAndDateModalOpen,
-		setIsUpdateTripDestinationAndDateModalOpen,
-	] = useState(false);
 
 	const openCreateActivityModal = () => {
 		setIsCreateActivityModalOpen(true);
@@ -23,21 +18,9 @@ export function TripDetailsPage() {
 		setIsCreateActivityModalOpen(false);
 	};
 
-	const openUpdateTripDestinationAndDateModal = () => {
-		setIsUpdateTripDestinationAndDateModalOpen(true);
-	};
-
-	const closeUpdateTripDestinationAndDateModal = () => {
-		setIsUpdateTripDestinationAndDateModalOpen(false);
-	};
-
 	return (
 		<div className="max-w-6xl px-6 py-10 mx-auto space-y-8">
-			<DestinationAndDateHeader
-				openUpdateTripDestinationAndDateModal={
-					openUpdateTripDestinationAndDateModal
-				}
-			/>
+			<DestinationAndDateHeader />
 			<main className="flex gap-16 px-4">
 				<Activities openCreateActivityModal={openCreateActivityModal} />
 				<menu className="w-80 space-y-6">
@@ -48,13 +31,6 @@ export function TripDetailsPage() {
 			</main>
 			{isCreateActivityModalOpen && (
 				<CreateActivityModal closeCreateActivityModal={closeCreateActivityModal} />
-			)}
-			{isUpdateTripDestinationAndDateModalOpen && (
-				<UpdateTripDestinationAndDateModal
-					closeUpdateTripDestinationAndDateModal={
-						closeUpdateTripDestinationAndDateModal
-					}
-				/>
 			)}
 		</div>
 	);
